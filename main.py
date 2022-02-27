9056c4618d9a3ba33ac67cd82929fbbf7dec3d76
Tonya Camille <tsims25@gmail.com>
2022-02-27 13:50:14 -0600

    More changes to main.py and players.html

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from ast import Bytes
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.websockets import WebSocketState
-from typing import Dict, Callable, List
+from typing import Dict, Callable
 from dotenv import load_dotenv
 from deepgram import Deepgram
 
 import os
 import re
-
-from main_old import connect_to_deepgram
 
 load_dotenv()
 
@@ -21,42 +18,43 @@ dg_client = Deepgram(os.getenv('DEEPGRAM_API_KEY'))
 templates = Jinja2Templates(directory="templates")
 
 
-async def process_audio(socket: WebSocket, data):
-    if 'channel' in data:
-        transcript = data['channel']['alternatives'][0]['transcript']
-    
-        if transcript:
-            await socket.send_text(transcript)
+async def process_audio(fast_socket: WebSocket):
+    async def get_transcript(data: Dict) -> None:
+        if 'channel' in data:
+            transcript = data['channel']['alternatives'][0]['transcript']
+        
+            if transcript:
+                await fast_socket.send_text(transcript)
+
+    deepgram_socket = await connect_to_deepgram(get_transcript)
+
+    return deepgram_socket
+
+async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]) -> str:
+    try:
+        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
+        socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+        socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
 
         return socket
-
-
-async def connect_to_deepgram():
-    def on_connection_closed(exit_code: int) -> int:
-        exit(0)
-
-    try:
-        deepgram_socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
-        deepgram_socket.registerHandler(deepgram_socket.event.CLOSE, on_connection_closed)
-        deepgram_socket.registerHandler(deepgram_socket.event.TRANSCRIPT_RECEIVED, print)
-
-        return deepgram_socket
-
     except Exception as e:
         raise Exception(f'Could not open socket: {e}')
-
-
 
 @app.get("/", response_class=HTMLResponse)
 def get(request: Request):
     return templates.TemplateResponse("players.html", {"request": request})
 
 @app.websocket("/listen")
-async def websocket_endpoint(websocket: WebSocket): # websocket that connects the client and server
+async def websocket_endpoint(websocket: WebSocket):  # websocket that connects the client and server
     await websocket.accept()
 
-    while True:
-        data = await websocket.receive_bytes() # while True receive bytes
-        deepgram_socket = await connect_to_deepgram() # connect to deepgram
-        await process_audio(deepgram_socket, data) # then process the audio
- 
+    try:
+        deepgram_socket = await process_audio(websocket) # then process the audio
+
+        while True:
+            data = await websocket.receive_bytes() # while True receive bytes
+            deepgram_socket.send(data)
+    except Exception as e:
+        raise Exception(f'Could not process audio: {e}')
+    finally:
+        await websocket.close()
